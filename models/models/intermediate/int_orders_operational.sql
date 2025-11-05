4e76eb55e1af3875f0de4a1301c0a26fd85f1774
Pauline Corthouts <corthouts.pauline@gmail.com>
2025-11-05 16:14:32 +0000

    add data to use ops in finance day

--- models/models/intermediate/int_orders_operational.sql
+++ models/models/intermediate/int_orders_operational.sql
@@ -6,8 +6,11 @@ orders_id,
 date_date,
 margin,
 shipping_fee,
+quantity,
 logcost,
 ship_cost,
+purchase_cost,
+revenue,
 FROM {{ ref('int_orders_margin') }} as o
 INNER JOIN {{ ref('stg_raw__ship') }} as s
 USING (orders_id)
@@ -16,6 +19,9 @@ SELECT
 orders_id,
 MAX(date_date) as date_date, 
 SUM(margin) as margin, 
+SUM(quantity) as quantity, 
+round(sum(purchase_cost),2) as purchase_cost, 
+SUM(revenue) as revenue,
 SUM(shipping_fee) as shipping_fee, 
 SUM(logcost) as log_cost, 
 SUM(ship_cost) as ship_cost,
