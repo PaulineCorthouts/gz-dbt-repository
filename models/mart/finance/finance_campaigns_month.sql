52ae6ecd2d720e18e0d30f802d89f80f583ea51f
Pauline Corthouts <corthouts.pauline@gmail.com>
2025-11-06 14:29:08 +0000

    order by month

---
+++ models/mart/finance/finance_campaigns_month.sql
@@ -0,0 +1,18 @@
+
+SELECT 
+FORMAT_DATE('%Y-%m', date_date) as datemonth,
+    revenue,
+    margin,
+    ads_cost,
+    ads_clicks,
+    ads_impression,
+    operational_margin,
+    ads_margin,
+    average_basket,
+    purchase_cost,
+    shipping_fee,
+    log_cost,
+    ship_cost,
+    quantity
+FROM {{ ref('finance_campaigns_day') }}
+
