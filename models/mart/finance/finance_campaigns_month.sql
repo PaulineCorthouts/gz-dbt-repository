1db3dd5af18586219e842d56a42a9eaedecdd32b
Pauline Corthouts <corthouts.pauline@gmail.com>
2025-11-07 15:24:49 +0000

    changed formula to aggregate in order to have a primary key

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,17 +1,17 @@
 SELECT 
 FORMAT_DATE('%Y-%m', date_date) as datemonth,
-    revenue,
-    margin,
-    ads_cost,
-    ads_clicks,
-    ads_impression,
-    operational_margin,
-    ads_margin,
-    average_basket,
-    purchase_cost,
-    shipping_fee,
-    log_cost,
-    ship_cost,
-    quantity
+    SUM(revenue) as revenue,
+    SUM(margin) as margin,
+    SUM(ads_cost) as ads_cost,
+    SUM(ads_clicks) as ads_clicks,
+    SUM(ads_impression) as ads_impression,
+    SUM(operational_margin) as operational_margin,
+    SUM(ads_margin) as ads_margin,
+    AVG(average_basket) as average_basket,
+    SUM(purchase_cost) as purchase_cost,
+    SUM(shipping_fee) as shipping_fee,
+    SUM(log_cost) as log_cost,
+    SUM(ship_cost) as ship_cost,
+    SUM(quantity) as quantity
 FROM {{ ref('finance_campaigns_day') }}
 
